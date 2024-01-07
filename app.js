a8d9b15f1738800793f988b8c87a516ea1a49b23
wes35 <lachand_michael@hotmail.com>
2024-01-07 22:31:26 +0100

    Event modifiers et v-once

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = Vue.createApp({
         return {
             counter: 0,
             name: '',
+            confirmedName: '',
         };
     },
     methods: {
+        confirmInput() {
+            this.confirmedName = this.name;
+        },
+        submitForm() {
+            alert('test form!');
+        },
         setName(event, lastName) {
             this.name = event.target.value + ' ' + lastName;
         },
